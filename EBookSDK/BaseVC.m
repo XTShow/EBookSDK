ac21ab46f58659bf4004c4fe74bc701f251f0f22
XTShow <ting.xiao@jsxfedu.com>
2017-07-25 17:02:01 +0800

    no message

---
+++ EBookSDK/BaseVC.m
@@ -0,0 +1,48 @@
+//
+//  BaseVC.m
+//  静态库Framework
+//
+//  Created by 肖霆 on 2017/7/5.
+//  Copyright © 2017年 Beijing JingShiXunFei Education Technologies Co.,Ltd. All rights reserved.
+//
+
+#import "BaseVC.h"
+#import <WebKit/WebKit.h>
+@interface BaseVC ()
+
+@end
+
+@implementation BaseVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+-(void)makeWeb{
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+    webView.allowsLinkPreview = NO;
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
+    
+    [self.view addSubview:webView];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
